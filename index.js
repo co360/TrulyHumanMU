07a35d0574235c33a12fd928ae134729395aff23
Maria Rohrmann <68588264+mariascode@users.noreply.github.com>
2020-09-01 16:19:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ categories.forEach(
 
 //animating the slideShow
 
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slidePic");
+  let i;
+  const slides = document.getElementsByClassName("slidePic");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
